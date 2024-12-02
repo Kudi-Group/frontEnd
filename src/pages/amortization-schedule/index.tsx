57fefc45b25d43a826d8e2c78ee57a50b7800d0a
unknown <chima.okwuokei@pau.edu.ng>
2024-12-02 22:51:31 +0100

    chore: work on routing

--- src/pages/amortization-schedule/index.tsx
+++ src/pages/amortization-schedule/index.tsx
@@ -1,95 +1,5 @@
-// import { useEffect, useState } from "react"
-// import { Payment, columns } from "./columns"
-// import { DataTable } from "./data-table"
-// import { Header } from "@/components/header";
-
-
-// async function getData(): Promise<Payment[]> {
-//     // Fetch data from your API here.
-//     const loanData = [
-//         {
-//             id: "1",
-//             dueDate: new Date("2024-01-01"),
-//             amount: 500,
-//             interest: 25,
-//             principal: 475,
-//             remainingBalance: 4525,
-//         },
-//         {
-//             id: "2",
-//             dueDate: new Date("2024-02-01"),
-//             amount: 500,
-//             interest: 23,
-//             principal: 477,
-//             remainingBalance: 4048,
-//         },
-//         {
-//             id: "3",
-//             dueDate: new Date("2024-02-02"),
-//             amount: 500,
-//             interest: 23,
-//             principal: 477,
-//             remainingBalance: 4048,
-//         },
-//     ];
-
-//     // Format dates and numbers dynamically
-//     const formattedLoanData = loanData.map((item) => ({
-//         id: item.id,
-//         dueDate: item.dueDate.toLocaleDateString("en-GB"), // Format as dd-MM-yyyy
-//         amount: item.amount.toFixed(2), // 2 decimal places
-//         interest: item.interest.toFixed(2),
-//         principal: item.principal.toFixed(2),
-//         remainingBalance: item.remainingBalance.toFixed(2),
-//     }));
-
-//     return formattedLoanData;
-
-// }
-
-// export function SchedulePage() {
-//     const [data, setData] = useState<Payment[]>([])
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             const result = await getData();
-//             setData(result);
-//         };
-
-//         fetchData(); // Call the async function inside useEffect
-//     }, []);
-
-//     const [loans, setLoans] = useState<Loan[]>([]);
-
-//     useEffect(() => {
-//         const getLoans = async () => {
-//           try {
-//             const res = await axios.get<UserLoansResponse>(`${baseUrl}/user/${user?.id}`);
-//             setLoans(res.data.loans);
-//           } catch (error) {
-//             console.error("Error fetching loan details:", error);
-//           }
-//         };
-    
-//         if (user?.id) {
-//           getLoans();
-//         }
-//       }, [user?.id]);
-
-//     const firstLoan = loans[1]; // Access the first loan
-
-
-//     return (
-//         <div className="xs:px-6 py-10 overflow-auto space-y-4">
-//             <Header />
-//             <div className="lg:px-8">
-//             <DataTable columns={columns} data={data} />
-//             </div>
-//         </div>
-//     )
-// }
 import { useEffect, useState } from "react";
-import { Payment, columns } from "./columns";
+import { columns } from "./columns";
 import { DataTable } from "./data-table";
 import { Header } from "@/components/header";
 import axios from "axios";
@@ -170,17 +80,6 @@ export function SchedulePage() {
         const { paymentLog } = res.data;
         console.log(paymentLog)
 
-        // Get the current month and year
-        // const currentMonth = new Date().getMonth();
-        // const currentYear = new Date().getFullYear();
-
-        // // Filter the paymentLog for the current month
-        // const filteredLog = paymentLog.filter((log) => {
-        //   const logDate = new Date(log.date);
-        //   return (
-        //     logDate.getMonth() === currentMonth && logDate.getFullYear() === currentYear
-        //   );
-        // });
 
         // Format filtered log data for the table
         const formattedData = paymentLog.map((log, index) => ({
